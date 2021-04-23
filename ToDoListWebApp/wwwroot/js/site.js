5c56febc319da66f8e3867bbe700fdad77b1c5d6
Utshab Khadka <khadkautshav7@gmail.com>
2021-04-23 19:14:32 -0400

    Added copy to clipboard

--- ToDoListWebApp/wwwroot/js/site.js
+++ ToDoListWebApp/wwwroot/js/site.js
@@ -2,3 +2,14 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+window.addEventListener('offline', function (event) {
+    $('#newtask').attr("disabled", "disabled");
+    $('#newtask').css('pointer-events', 'none');
+});
+window.addEventListener(‘online’, function (event) {
+    $('#newtask').removeAttr('disabled');
+    $('#newtask').css('pointer-events', 'auto');
+});
+function myFunction() {
+    alert("Hello World!");
+}
